c536089f17f8cdcfda65bda19000555c581c69af
u-sho <26079835+u-sho@users.noreply.github.com>
2022-08-14 00:38:49 +0900

    🎨 LocalHost static files

--- socket-chat-example/index.js
+++ socket-chat-example/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import http from "http";
 
 const app = express();
+app.use(express.static("public"));
+
 const server = http.createServer(app);
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(process.env.npm_config_local_prefix + "/index.html");
+// });
 
 server.listen(3000, () => {
   console.log("Server listening on port 3000");
