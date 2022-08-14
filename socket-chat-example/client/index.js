dcab9428442e3cb4e2cc455d149698f18c891d6e
u-sho <26079835+u-sho@users.noreply.github.com>
2022-08-15 00:28:49 +0900

    🗣️ receive messages: youtube style chat

--- socket-chat-example/client/index.js
+++ socket-chat-example/client/index.js
@@ -1,12 +1,22 @@
 const socket = io();
 
+const messages = document.getElementsByTagName("ul")?.[0];
 const form = document.getElementsByTagName("form")?.[0];
 const input = document.getElementsByTagName("input")?.[0];
 
+// send message to socket server
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (input.value) {
     socket.emit("chat message", input.value);
     input.value = "";
   }
+});
+
+// receive message from socket server
+socket.on("chat message", (msg) => {
+  const item = document.createElement("li");
+  item.textContent = msg;
+  messages.appendChild(item);
+  messages.scrollTo(0, messages.scrollHeight);
 });
--- socket-chat-example/index.js
+++ socket-chat-example/index.js
@@ -8,6 +8,9 @@ app.use(express.static("client"));
 const httpServer = http.createServer(app);
 const socketIO = new Server(httpServer);
 
+httpServer.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
 app.get("/", (req, res) => {
   res.sendFile(process.env.npm_config_local_prefix + "/index.html");
 });
@@ -18,11 +21,8 @@ socketIO.on("connection", (socket) => {
     console.log("user disconnected");
   });
 
+  // broadcast messages to all connected clients
   socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
+    socketIO.emit("chat message", msg);
   });
-});
-
-httpServer.listen(3000, () => {
-  console.log("Server listening on port 3000");
 });
